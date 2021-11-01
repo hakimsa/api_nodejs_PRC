8eb7abfe68fc126936a6a6bbe04acd3a2a4ad936
samou <samouh591@gmail.com>
2021-11-01 19:31:35 +0100

    add passport strategy

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let conexion = require('./Services/Conexion.js')
 const UserController = require('./Controllers/UserController');
 const app = express();
 const Puerto = 7766;
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+require("dotenv").config();
 
 // midle ware
 app.use((req, res, next) => {
@@ -28,3 +32,48 @@ app.post('/user', UserController.saveUser);
 app.put('/user/:id', UserController.updateUser);
 app.delete('/user/:id', UserController.deleteUser);
 app.listen(process.env.PORT || Puerto)
+
+
+/**
+ * Session Configuration
+ */
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy({
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
